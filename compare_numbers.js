411430c618e843e2db0636aad7527a329376be8d
kurtlin <kurtlin.hendricks@umuzi.org>
2019-11-19 09:59:48 +0200

    first intake

---
+++ compare_numbers.js
@@ -0,0 +1,29 @@
+function bigAndSmall(num1, num2, num3){
+
+
+
+if(num1 > num2 && num1 > num3){
+console.log(num1)
+
+}else if (num2 > num1 && num2 > num3){
+	console.log(num2)
+}
+else{
+	console.log(num3)
+}
+
+
+if(num1 < num2 && num1 < num3){
+console.log(num1)
+
+}else if (num2 < num1 && num2 < num3){
+	console.log(num2)
+}
+else{
+	console.log(num3)
+}
+}
+
+bigAndSmall(21, 45, 15)
+
+
---
+++ odd_numbers.js
@@ -0,0 +1,13 @@
+function oddNumbers() {
+startpoint = 1;
+
+while(startpoint < 10){
+	
+console.log(startpoint);
+	startpoint = startpoint + 2;
+	
+//return (startpoint)
+}
+}
+//console.log(oddNumbers(1,2,3,4,5,6,8,9,10))
+oddNumbers()
